8a1b4b89a00546a13f313a177772ca48ebec7c24
Thomas Syczyk <tomsycz@gmail.com>
2021-04-14 17:16:34 +0100

    basic app design

--- face-recognition/src/App.js
+++ face-recognition/src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Navigation from "./components/Navigation/Navigation";
+import Logo from "./components/Logo/Logo";
+import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
+import Rank from "./components/Rank/Rank";
+import Particles from "react-particles-js";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import "./App.css";
+
+const particlesOptions = {
+  particles: {
+    number: {
+      value: 10,
+      density: {
+        enable: true,
+        value_area: 200,
+      },
+    },
+  },
+  interactivity: {
+    onhover: {
+      enable: true,
+      mode: "grab",
+    },
+  },
+};
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Particles params={particlesOptions} className="particles" />
+        <Navigation />
+        <Logo />
+        <Rank />
+        <ImageLinkForm />
+
+        {/* <FaceRecognition /> */}
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ face-recognition/src/components/Logo/Logo.js
@@ -0,0 +1,28 @@
+import React from "react";
+import Tilt from "react-parallax-tilt";
+import "./Logo.css";
+import face from "./face.png";
+
+const Logo = () => {
+  return (
+    <div className="ma4 mt0">
+      <Tilt
+        className="Tilt br2 shadow-2"
+        tiltReverse={false}
+        perspective={1000}
+        glareEnable={true}
+        glareMaxOpacity={0.1}
+        glarePosition="all"
+        scale={1.1}
+        trackOnWindow={false}
+        glareBorderRadius="5px"
+      >
+        <div className="inner-element pa3">
+          
+          <img style={{ paddingTop: "5px" }} src={face} alt="logo" />
+        </div>
+      </Tilt>
+    </div>
+  );
+};
+export default Logo;
